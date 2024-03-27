34b045da8ade053cfab37fb7e15a6b7605939856
hynekpihera <hynek.pihera@gpjp.cz>
2024-03-27 13:30:44 +0100

    init

---
+++ kostka.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#hynkuv code
+import  random
+
+class Kostka:   
+
+    def __init__(self, pocet sten):
+        self.pocetSten=pocet_sten
+   
+    def hod(self):
+        return random.randint(1, self.pocetSten)
+
+    def getPocetSten(self):
+        return self.__pocetSten
+
+    def setPocetSten(self, pocet_sten):
+        if pocet sten 
+
+
+k1 = Kostka(6)
+print(k1.hod())
+
+k2 = Kostka(12)
+print(k2.hod())
+
+print(k1.getPocetSten())
+k1.setPocetSten(-5)
+
+
+
+    
+
+
+ 
+
+
